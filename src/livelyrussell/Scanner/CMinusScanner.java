baae97882be0795c5204b96dc34407eb72b38950
Jesse <Jesse@JessePC>
2017-02-07 16:57:01 -0500

    Finished up first pass of the project. Working on outputting to file.

--- src/livelyrussell/Scanner/CMinusScanner.java
+++ src/livelyrussell/Scanner/CMinusScanner.java
@@ -6,13 +6,21 @@
 package livelyrussell.Scanner;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import static java.lang.Character.isDigit;
 import static java.lang.Character.isLetter;
 import static java.lang.Character.isWhitespace;
 import static java.lang.Character.isDigit;
 import static java.lang.Character.isLetter;
 import static java.lang.Character.isWhitespace;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,8 +29,10 @@ import static java.lang.Character.isWhitespace;
 public class CMinusScanner implements Scanner {
 
     private BufferedReader inFile;
+    private File outFile;
     private Token nextToken;
     private final static char EOF = (char) -1;
+    private static HashMap map = new HashMap();
 
     private enum State {
         //Movement
@@ -38,6 +48,14 @@ public class CMinusScanner implements Scanner {
     public CMinusScanner(BufferedReader file) throws IOException {
         inFile = file;
         nextToken = scanToken();
+        outFile = new File("output.txt");
+
+        map.put("else", Token.TokenType.ELSE);
+        map.put("if", Token.TokenType.IF);
+        map.put("int", Token.TokenType.INT);
+        map.put("return", Token.TokenType.RETURN);
+        map.put("void", Token.TokenType.VOID);
+        map.put("while", Token.TokenType.WHILE);
     }
 
     @Override
@@ -57,7 +75,7 @@ public class CMinusScanner implements Scanner {
     public Token scanToken() throws IOException {
 
         State state = State.START;
-        Token.TokenType type;
+        Token.TokenType type = Token.TokenType.ERROR;
         //Default token type should be changed later on.
         Token token = new Token(Token.TokenType.ERROR);
         boolean save;
@@ -232,33 +250,113 @@ public class CMinusScanner implements Scanner {
                     type = Token.TokenType.ERROR;
                     break;
             }
-            
-            if(save){
-                if(state == State.IN_NUM){
-                    if(isDigit(c)){
-                       String temp = (String) token.viewData();
-                       token.setData((Object) (temp + String.valueOf(c)));
-                    }else{
-                        inFile.reset();
+
+            if (save) {
+                String temp;
+                temp = (String) token.viewData();
+                token.setData((Object) (temp + String.valueOf(c)));
+                break;
+            }
+            if (state == State.DONE) {
+                token.setType(type);
+                if (token.viewType() == Token.TokenType.NUM) {
+                    Integer holder = Integer.parseInt((String) token.viewData());
+                    token.setData(holder);
+                } else if (token.viewType() == Token.TokenType.ID) {
+                    String holder = (String) token.viewData();
+                    type = (Token.TokenType) map.get(holder);
+                    if (type != null) {
+                        token.setType(type);
                     }
-                }else if(state == State.IN_ID){
-                    if(isLetter(c)){
-                        String temp = (String) token.viewData();
-                        token.setData((Object) (temp + String.valueOf(c)));
-                    }
-                    else{
-                        inFile.reset();
-                    }
-                }else if(state == State.IN_LT){
-                    
                 }
             }
-            
         }
-        return new Token(Token.TokenType.IF);//Garbage
+
+        return token;
+    }
+
+    private void printToken(Token token) throws FileNotFoundException {
+        PrintStream ps = new PrintStream(outFile);
+        switch (token.viewType()) {
+            case IF:
+            case ELSE:
+            case INT:
+            case RETURN:
+            case VOID:
+            case WHILE:
+                ps.printf("reserved word: %s\n", token.viewData());
+            case PLUS:
+                ps.printf("+\n");
+            case MINUS:
+                ps.printf("-\n");
+            case STAR:
+                ps.printf("*\n");
+            case SLASH:
+                ps.printf("/\n");
+            case ASSIGN:
+                ps.printf("=\n");
+            case SEMICOLON:
+                ps.printf(";\n");
+            case COMMA:
+                ps.printf(",\n");
+            case GREATER_THAN:
+                ps.printf(">\n");
+            case GREATER_EQUAL:
+                ps.printf(">=\n");
+            case LESS_THAN:
+                ps.printf("<\n");
+            case LESS_EQUAL:
+                ps.printf("<=\n");
+            case EQUAL:
+                ps.printf("==\n");
+            case NOT_EQUALS:
+                ps.printf("!=\n");
+            case LEFTPAREN:
+                ps.printf("(\n");
+            case RIGHTPAREN:
+                ps.printf(")\n");
+            case LEFTSQUARE:
+                ps.printf("[\n");
+            case RIGHTSQUARE:
+                ps.printf("]\n");
+            case LEFTCURLY:
+                ps.printf("{\n");
+            case RIGHTCURLY:
+                ps.printf("}\n");
+            case ID:
+                ps.printf("ID, name = %s\n", token.viewData());
+            case NUM:
+                ps.printf("NUM, value = %i\n", token.viewData());
+            case ERROR:
+                ps.printf("ERROR: %s\n", token.viewData());
+            case EOF:
+                ps.printf("EOF");
+            default:
+                ps.printf("wat");
+        }
     }
 
     public static void main(String args[]) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("File Name:");
+        String filename = new String();
+        BufferedReader file;
+
+        try {
+            //get filename
+            filename = br.readLine();
+            //create buffered reader BufferedReader file;
+            file = new BufferedReader(new FileReader(filename));
+
+            CMinusScanner cms = new CMinusScanner(file);
+            while (cms.viewNextToken().viewType() != Token.TokenType.EOF) {
+                cms.printToken(cms.getNextToken());
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(CMinusScanner.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
