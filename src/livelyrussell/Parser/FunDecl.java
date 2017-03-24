5cd3b703adfff5a5f8dd6b51735443860bc64718
Richard Lively <richielively@gmail.com>
2017-03-24 13:41:18 -0400

    Working on funDecl print

--- src/livelyrussell/Parser/FunDecl.java
+++ src/livelyrussell/Parser/FunDecl.java
@@ -2,24 +2,39 @@ package livelyrussell.Parser;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class FunDecl extends Declaration{
-
-    @Override
-    public void print(PrintStream out) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     public enum type{
         VOID, INT
     }
-    private FunDecl.type kind;
-    private CompoundStmt cs;
-    private ArrayList<Param> params;
+    private final FunDecl.type kind;
+    private final CompoundStmt cs;
+    private final ArrayList<Param> params;
 
     FunDecl(String i, CompoundStmt c, ArrayList<Param> p, FunDecl.type t) {
         super(i);
-        kind = t;
         cs = c;
         params = p;
+        kind = t;
+    }
+    
+    @Override
+    public void print(PrintStream out) {
+        out.println("FunDecl: " + kind + " " + id);
+        if (params.isEmpty()) {
+            out.println("\t()");
+        }
+        else {
+            out.println("\t(");
+            for (Iterator<Param> it = params.iterator(); it.hasNext();) {
+                Param param = it.next();
+                out.print("\t\t");
+                param.print(out);
+            }
+            out.println("\t)");
+        }
+        out.print("\t");
+        
     }
 }
