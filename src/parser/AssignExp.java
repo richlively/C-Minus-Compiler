edc9f91a37f98355d59fd849f4f16e9664127760
Jesse <Jesse@JessePC>
2017-05-04 22:34:08 -0400

    I think i got AssignExp working

--- src/parser/AssignExp.java
+++ src/parser/AssignExp.java
@@ -1,13 +1,15 @@
 package parser;
 
+import compiler.CMinusCompiler;
 import java.io.PrintStream;
 import lowlevel.CodeItem;
 import lowlevel.Function;
+import lowlevel.Operand;
+import lowlevel.Operation;
 
 public class AssignExp extends Expression {
 
     //first thing on right side of line 18 on page 492
-
     private VarExp var;
     private Expression exp;
 
@@ -27,9 +29,26 @@ public class AssignExp extends Expression {
         var.print(out, indent + 1);
         exp.print(out, indent + 1);
     }
-    
+
     @Override
     public int genLLCode(Function fun) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int v = var.genLLCode(fun);
+        String varid = var.getID();
+        int e = var.genLLCode(fun);
+        if (CMinusCompiler.globalHash.containsKey(varid)) {
+            Operation oper = new Operation(Operation.OperationType.STORE_I, fun.getCurrBlock());
+            Operand src = new Operand(Operand.OperandType.REGISTER, e);
+            Operand dest = new Operand(Operand.OperandType.STRING, varid);
+            oper.setSrcOperand(0, src);
+            oper.setDestOperand(0, dest);
+            return e;
+        } else {
+            Operation oper = new Operation(Operation.OperationType.ASSIGN, fun.getCurrBlock());
+            Operand src = new Operand(Operand.OperandType.REGISTER, e);
+            Operand dest = new Operand(Operand.OperandType.REGISTER, v);
+            oper.setSrcOperand(0, src);
+            oper.setDestOperand(0, dest);
+            return v;
+        }
     }
 }
